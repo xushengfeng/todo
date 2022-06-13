900875b42ee319974a9050d8b3e75b4a985a6196
xushengfeng <xushengfeng_zg@163.com>
2022-06-13 20:46:19 +0800

    :sparkles:主要 保存数据为文件

--- js/js.js
+++ js/js.js
@@ -6,3 +6,24 @@ function r() {
 }
 
 setInterval(r, 500);
+
+function save() {
+    let o = {};
+    let item = document.querySelectorAll("item-b");
+    for (let i of item) {
+        if (i.text)
+            o[i.id] = {
+                time: i.time,
+                text: i.text,
+                list: i.list,
+                date: i.text,
+            };
+    }
+
+    let a = document.createElement("a");
+    let blob = new Blob([JSON.stringify(o)]);
+    a.download = "todo_list.json";
+    a.href = URL.createObjectURL(blob);
+    a.click();
+    URL.revokeObjectURL(String(blob));
+}
